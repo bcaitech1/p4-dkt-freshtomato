0e228005e924c8518fcb6e9032ad512bc33a5fe0
arabae <arbae@inu.ac.kr>
2021-05-25 12:31:07 +0000

    run black in dkt

--- code/dkt/model.py
+++ code/dkt/model.py
@@ -1,20 +1,21 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F 
+import torch.nn.functional as F
 import numpy as np
 import copy
 import math
 
 try:
-    from transformers.modeling_bert import BertConfig, BertEncoder, BertModel    
+    from transformers.modeling_bert import BertConfig, BertEncoder, BertModel
 except:
-    from transformers.models.bert.modeling_bert import BertConfig, BertEncoder, BertModel    
-
-
+    from transformers.models.bert.modeling_bert import (
+        BertConfig,
+        BertEncoder,
+        BertModel,
+    )
 
 
 class LSTM(nn.Module):
-
     def __init__(self, args):
         super(LSTM, self).__init__()
         self.args = args
@@ -23,37 +24,32 @@ class LSTM(nn.Module):
         self.hidden_dim = self.args.hidden_dim
         self.n_layers = self.args.n_layers
 
-        # Embedding 
+        # Embedding
         # interaction은 현재 correct로 구성되어있다. correct(1, 2) + padding(0)
-        self.embedding_interaction = nn.Embedding(3, self.hidden_dim//3)
-        self.embedding_test = nn.Embedding(self.args.n_test + 1, self.hidden_dim//3)
-        self.embedding_question = nn.Embedding(self.args.n_questions + 1, self.hidden_dim//3)
-        self.embedding_tag = nn.Embedding(self.args.n_tag + 1, self.hidden_dim//3)
+        self.embedding_interaction = nn.Embedding(3, self.hidden_dim // 3)
+        self.embedding_test = nn.Embedding(self.args.n_test + 1, self.hidden_dim // 3)
+        self.embedding_question = nn.Embedding(
+            self.args.n_questions + 1, self.hidden_dim // 3
+        )
+        self.embedding_tag = nn.Embedding(self.args.n_tag + 1, self.hidden_dim // 3)
 
         # embedding combination projection
-        self.comb_proj = nn.Linear((self.hidden_dim//3)*4, self.hidden_dim)
+        self.comb_proj = nn.Linear((self.hidden_dim // 3) * 4, self.hidden_dim)
 
-        self.lstm = nn.LSTM(self.hidden_dim,
-                            self.hidden_dim,
-                            self.n_layers,
-                            batch_first=True)
-        
+        self.lstm = nn.LSTM(
+            self.hidden_dim, self.hidden_dim, self.n_layers, batch_first=True
+        )
+
         # Fully connected layer
         self.fc = nn.Linear(self.hidden_dim, 1)
 
         self.activation = nn.Sigmoid()
 
     def init_hidden(self, batch_size):
-        h = torch.zeros(
-            self.n_layers,
-            batch_size,
-            self.hidden_dim)
+        h = torch.zeros(self.n_layers, batch_size, self.hidden_dim)
         h = h.to(self.device)
 
-        c = torch.zeros(
-            self.n_layers,
-            batch_size,
-            self.hidden_dim)
+        c = torch.zeros(self.n_layers, batch_size, self.hidden_dim)
         c = c.to(self.device)
 
         return (h, c)
@@ -70,12 +66,16 @@ class LSTM(nn.Module):
         embed_test = self.embedding_test(test)
         embed_question = self.embedding_question(question)
         embed_tag = self.embedding_tag(tag)
-        
 
-        embed = torch.cat([embed_interaction,
-                           embed_test,
-                           embed_question,
-                           embed_tag,], 2)
+        embed = torch.cat(
+            [
+                embed_interaction,
+                embed_test,
+                embed_question,
+                embed_tag,
+            ],
+            2,
+        )
 
         X = self.comb_proj(embed)
 
@@ -87,4 +87,3 @@ class LSTM(nn.Module):
         preds = self.activation(out).view(batch_size, -1)
 
         return preds
-
