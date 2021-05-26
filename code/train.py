28f10a6d3ca3edf5c8056c3e26057cdc451af488
MoonJaeHoon <jh951229@naver.com>
2021-05-26 15:06:37 +0000

    In train.py, Edit wandb_project_name

--- code/train.py
+++ code/train.py
@@ -20,7 +20,7 @@ def main(args):
 
     train_data, valid_data = preprocess.split_data(train_data)
 
-    wandb.init(project="freshtomato", config=vars(args))
+    wandb.init(project=args.wandb_project_name, config=vars(args))
 
     if args.wandb_run_name:
         wandb.run.name = args.wandb_run_name
