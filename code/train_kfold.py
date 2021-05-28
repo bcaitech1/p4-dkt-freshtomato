67f1970d7c06236847051677ce852fe916817724
arabae <arbae@inu.ac.kr>
2021-05-28 08:13:56 +0000

    change stratified kfold -> kfold

--- code/train_kfold.py
+++ code/train_kfold.py
@@ -27,9 +27,8 @@ def main(args):
     train_all_data = preprocess.get_train_data()
 
     wandb.login()
-    user_stratified_key = preprocess.get_user_stratified_key()
-
-    skf = StratifiedKFold(n_splits=args.kfold, shuffle=True, random_state=0)
+    
+    kf = KFold(n_splits=args.kfold, shuffle=True, random_state=args.seed)
     fold_counter = 0
 
     args.model_dir = os.path.join(args.model_dir, args.wandb_run_name)
@@ -44,7 +43,7 @@ def main(args):
     # fold별 acc&auc와 average acc&auc를 저장할 dictionary
     final_score_dict = {}
     # K-Fold 학습 시작
-    for train_idx, test_idx in skf.split(train_all_data, user_stratified_key):
+    for train_idx, test_idx in kf.split(train_all_data):
         args.fold = fold_counter
 
         train_data = train_all_data[train_idx]
